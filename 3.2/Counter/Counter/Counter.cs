9228896f5ddec9993f4bb55ed876cf325b9bd69a
Raz <razbasnet37@gmail.com>
2020-08-10 13:00:54 +1000

    Week 3 Task

---
+++ 3.2/Counter/Counter/Counter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Counter
+{
+    public class Counter
+    {
+        private int _count;
+        // private cannot be accessed outside of the object of Class Counter
+        private string _name;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+        }
+
+        public Counter(string name)
+        {
+            _name = name;
+            _count = 0;
+
+        }
+
+        public void Increment()
+        {
+            _count++;
+        }
+
+        public void Reset()
+        {
+            _count = 0;
+        }
+    }
+}
